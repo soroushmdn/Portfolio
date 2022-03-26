0e36e814cc0c7aecb537ae96781098a887588009
soroushmdn <sorooshkarimi93@gmail.com>
2022-03-26 18:59:41 +0430

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,10 @@
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector("nav .container ul");
+
+menu.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+});
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
